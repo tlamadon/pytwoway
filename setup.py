d2cafb67a847fae55a43eeed5d60b58eaddf36b1
Adam Oppenheimer <adam.a.oppenheimer@gmail.com>
2021-01-15 17:59:10 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open('README.rst', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='pytwoway',
-    version='0.0.4',
+    version='0.1.1',
     author='Thibaut Lamadon',
     author_email='thibaut.lamadon@gmail.com',
     description='Estimate two way fixed effect labor models',
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type='text/x-rst',
     url='https://github.com/tlamadon/pytwoway',
     packages=setuptools.find_packages(),
     install_requires=[
