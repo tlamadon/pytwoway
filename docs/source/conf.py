5c7444cf50a4b5175ab15fa7a18f4e37f694b781
Adam Oppenheimer <adam.a.oppenheimer@gmail.com>
2022-05-10 17:15:27 -0500

    New pip version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2021, Thibaut Lamadon.'
 author = 'Thibaut Lamadon'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.3'
+release = '0.2.4'
 
 
 # -- General configuration ---------------------------------------------------
