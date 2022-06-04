011e981af515c62add2fedfe60537633cb55f252
Adam Oppenheimer <adam.a.oppenheimer@gmail.com>
2022-06-03 21:45:28 -0500

    Version 0.2.14 [ci skip]

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2021, Thibaut Lamadon'
 author = 'Thibaut Lamadon'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.13'
+release = '0.2.14'
 
 
 # -- General configuration ---------------------------------------------------
