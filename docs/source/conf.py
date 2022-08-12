463484cb0248b7754a60fe323a63be33452e2727
adamoppenheimer <adam.a.oppenheimer@gmail.com>
2022-08-12 18:56:28 -0500

    Pip version 0.3.7 [ci skip]

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2021, Thibaut Lamadon'
 author = 'Thibaut Lamadon'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.6'
+release = '0.3.7'
 
 
 # -- General configuration ---------------------------------------------------
