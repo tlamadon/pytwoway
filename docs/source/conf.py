ed32c8046e78668f639e020355087064b5763677
adamoppenheimer <adam.a.oppenheimer@gmail.com>
2022-09-05 21:47:25 -0500

    Pip version 0.3.16

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2021, Thibaut Lamadon'
 author = 'Thibaut Lamadon'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.15'
+release = '0.3.16'
 
 
 # -- General configuration ---------------------------------------------------
