334f5c6434702bc4bc7999fdb740c679a92d432d
adamoppenheimer <adam.a.oppenheimer@gmail.com>
2022-09-09 22:37:20 -0500

    Pip version 0.3.19

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2021, Thibaut Lamadon'
 author = 'Thibaut Lamadon'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.18'
+release = '0.3.19'
 
 
 # -- General configuration ---------------------------------------------------
