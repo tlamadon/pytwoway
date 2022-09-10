e4650c02c3911e0a4f4fc1ee6a17d69e6b6523a8
adamoppenheimer <adam.a.oppenheimer@gmail.com>
2022-09-09 22:01:20 -0500

    Pip version 0.3.18

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='pytwoway',
-    version='0.3.17',
+    version='0.3.18',
     author='Thibaut Lamadon',
     author_email='thibaut.lamadon@gmail.com',
     description='Estimate two way fixed effect labor models',
