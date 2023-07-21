129b378b3cfeeacbaddb567db5e095d41245195c
adamoppenheimer <adam.a.oppenheimer@gmail.com>
2023-07-21 22:04:04 +0800

    Add plot of firm class proportions by category for static/dynamic BLM; rename function to plot firm class proportions over time for static/dynamic BLM [ci skip]

--- pytwoway/diagnostics/__init__.py
+++ pytwoway/diagnostics/__init__.py
@@ -1,4 +1,4 @@
 
 from .plot_earnings_asymmetry import plot_earnings_asymmetry
 from .plot_extendedeventstudy import plot_extendedeventstudy_params, plot_extendedeventstudy
-from .plot_firm_classes_over_time import plot_firm_classes_over_time
+from .plot_firm_classes import plot_firm_class_proportions_over_time, plot_firm_class_proportions_by_category
