Fix weighted var(eps) computation for FE [ci skip]

pytwoway/fe.py
@@ -73,7 +73,7 @@ fe_params = ParamsDict({
         ''', None),
     'uncorrelated_errors': (False, 'type', bool,
         '''
-            (default=False) Set to True if using weighted estimator and errors are assumed to be uncorrelated within job spells.
+            (default=False) Set to True if using weighted estimator and errors are assumed to be uncorrelated within job spells. Set to False if using weighted estimator and errors are assumed to be perfectly correlated within job spells.
         ''', None),
     'Q_var': (None, 'list_of_type_none', (Q.VarPsi, Q.VarAlpha, Q.VarPsiPlusAlpha),
         '''
@@ -573,7 +573,7 @@ class FEEstimator:
         # self.res['movers_per_firm'] = self.adata.loc[self.adata.loc[:, 'm'] > 0, :].groupby('j')['i'].nunique().mean()
         self.res['between_firm_var'] = weighted_var(fy, fi)
         # NOTE: set duplicated_values=False for uncorrelated errors, because each observation's variance is divided by Dp and we need to undo that
-        self.res['var(y)'] = weighted_var(self.Y, self.Dp, duplicated_values=(not self.params['uncorrelated_errors']))
+        self.res['var(y)'] = weighted_var(self.Y, self.Dp)
         self.logger.info(f"total variance: {self.res['var(y)']:2.4f}")
 
         # extract woodcock moments using sdata and jdata
@@ -629,9 +629,12 @@ class FEEstimator:
         ## Estimate residuals ##
         self.E = self.Y - self._mult_A(self.psi_hat, self.alpha_hat, weighted=False)
 
-        ## Estimate variance of residuals (DON'T DEMEAN) ##
-        # NOTE: set duplicated_values=False for uncorrelated errors, because each observation's variance is divided by Dp and we need to undo that
-        self.sigma_2_fe = weighted_mean(self.E ** 2, self.Dp, duplicated_values=(not self.params['uncorrelated_errors']))
+        ## Estimate variance of residuals (DON'T DEMEAN) (USE WEIGHT ONLY FOR SCALING) ##
+        if not self.params['uncorrelated_errors']:
+            self.sigma_2_fe = np.mean(self.E ** 2)
+        else:
+            # NOTE: multiply by Dp for uncorrelated errors, because each observation's variance is divided by Dp and we need to undo that
+            self.sigma_2_fe = np.mean(self.Dp * (self.E ** 2))
 
         ## Estimate R^2 ##
         fe_rsq = 1 - self.sigma_2_fe / self.res['var(y)']
@@ -1719,9 +1722,12 @@ class FEEstimator:
                     # Multiply by bias correction factor
                     Sii *= jla_factor
 
-        # Compute sigma^2 HE
-        # NOTE: set duplicated_values=False for uncorrelated errors, because each observation's variance is divided by Dp and we need to undo that
-        self.res['var(eps)_he'] = weighted_mean(Sii, w, duplicated_values=(not self.params['uncorrelated_errors']))
+        # Compute sigma^2 HE (DON'T DEMEAN) (USE WEIGHT ONLY FOR SCALING)
+        if not self.params['uncorrelated_errors']:
+            self.res['var(eps)_he'] = np.mean(Sii)
+        else:
+            # NOTE: multiply by w for uncorrelated errors, because each observation's variance is divided by w and we need to undo that
+            self.res['var(eps)_he'] = np.mean(w * Sii)
 
         self.logger.info(f"[he] variance of residuals {self.res['var(eps)_he']:2.4f}")
 

pytwoway/util.py
@@ -7,23 +7,20 @@ from bipartitepandas.util import to_list
 from scipy.sparse import csc_matrix
 from matplotlib import pyplot as plt
 
-def weighted_mean(v, w=None, duplicated_values=True):
+def weighted_mean(v, w=None):
     '''
     Compute weighted mean.
 
     Arguments:
         v (NumPy Array): vector to weight
         w (NumPy Array or float or None): weights; None is equivalent to no weights
-        duplicated_values (bool): if True, weights indicate duplicated values (standard weighted mean); otherwise, weights indicate a weighted average over multiple observations
 
     Returns:
         (NumPy Array): weighted mean
     '''
     if (w is None) or isinstance(w, (float, int)):
         return np.mean(v)
-    if duplicated_values:
-        return np.sum(w * v) / np.sum(w)
-    return np.sum((w ** 2) * v) / np.sum(w)
+    return np.sum(w * v) / np.sum(w)
 
 def weighted_var(v, w=None, duplicated_values=True, dof=0):
     '''
@@ -45,10 +42,10 @@ def weighted_var(v, w=None, duplicated_values=True, dof=0):
         return np.sum((v - m0) ** 2) / (n - dof)
 
     if duplicated_values:
-        w2 = w
+        return np.sum(w * (v - m0) ** 2) / (np.sum(w) - dof)
     else:
-        w2 = w ** (3 / 2)
-    return np.sum(w2 * (v - m0) ** 2) / (np.sum(w) - dof)
+        # NOTE: use weights only to scale
+        return np.sum(w * (v - m0) ** 2) / (len(v) - dof)
 
 def weighted_cov(v1, v2, w1=1, w2=1, duplicated_values=True, dof=0):
     '''
@@ -72,14 +69,12 @@ def weighted_cov(v1, v2, w1=1, w2=1, duplicated_values=True, dof=0):
         n = len(v1)
         return np.sum((v1 - m1) * (v2 - m2)) / (n - dof)
 
+    w3 = np.sqrt(w1 * w2)
     if duplicated_values:
-        w3 = np.sqrt(w1 * w2)
-        w4 = w3
+        return np.sum(w3 * (v1 - m1) * (v2 - m2)) / (np.sum(w3) - dof)
     else:
-        w3 = np.sqrt(w1 * w2)
-        w4 = (w1 * w2) ** (3 / 4)
-
-    return np.sum(w4 * (v1 - m1) * (v2 - m2)) / (np.sum(w3) - dof)
+        # NOTE: use weights only to scale
+        return np.sum(w3 * (v1 - m1) * (v2 - m2)) / (len(v1) - dof)
 
 def weighted_quantile(values, quantiles, sample_weight=None, values_sorted=False, old_style=False):
     '''